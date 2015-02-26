9ed990d467c113f7dac537c4bcca8fb9368b2b70
A502197 <A502197@EIT12006.af.se>
2015-02-26 10:56:40 +0100

    test

---
+++ src/main/java/core/HelloWorld.java
@@ -0,0 +1,15 @@
+package core;
+
+public class HelloWorld {
+	
+	private static final String s = "Hello World!";
+	
+	public static String sayHello(){return s;}
+
+	public static void main(String[] args) {
+
+		System.out.println(HelloWorld.sayHello());
+
+	}
+
+}
---
+++ src/test/java/core/TestHelloWorld.java
@@ -0,0 +1,15 @@
+package core;
+
+import static org.junit.Assert.*;
+import static core.HelloWorld.*;
+
+import org.junit.Test;
+
+public class TestHelloWorld {
+
+	@Test
+	public void test() {
+		assertTrue("Strings are equal", "Hello World!".equals(sayHello()));
+	}
+
+}
